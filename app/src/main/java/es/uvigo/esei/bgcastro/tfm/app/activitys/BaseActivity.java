25f56bcf2efa43028d01d30465d11e784427dbca
braisgallegocastro <bgcastro@esei.uvigo.es>
2016-06-22 20:33:46 +0200

    Comentarios añadidos

--- app/src/main/java/es/uvigo/esei/bgcastro/tfm/app/activitys/BaseActivity.java
+++ app/src/main/java/es/uvigo/esei/bgcastro/tfm/app/activitys/BaseActivity.java
@@ -11,16 +11,27 @@ import es.uvigo.esei.bgcastro.tfm.R;
 
 /**
  * Created by braisgallegocastro on 10/2/16.
+ * Activity para reuninir los elementos comunes de todos las activities
  */
 public class BaseActivity extends AppCompatActivity {
 
     private static final String TAG = "BaseActivity";
 
+    /**
+     * onCreate
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+    /**
+     * onCreateOptionsMenu
+     * Inicializa el menu
+     *
+     * @param menu Menu en el que colocar items.
+     * @return Boolean para mostrar el menu.
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.common_menu, menu);
@@ -28,6 +39,12 @@ public class BaseActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+    /**
+     * onOptionsItemSelected
+     * LLamado cuando se pulsa un item
+     * @param item Item seleccionado
+     * @return true si se ha atendido la accion.
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -48,6 +65,10 @@ public class BaseActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * abrirOpciones
+     * Funcion para abrir las preferencias
+     */
     private void abrirOpciones() {
         Log.d(TAG, "abrirOpciones: ");
 
@@ -55,6 +76,10 @@ public class BaseActivity extends AppCompatActivity {
         startActivity(intentPreferencias);
     }
 
+    /**
+     * buscarOpiniones
+     * Funcion para abrir la activity de buscar opiniones
+     */
     private void buscarOpiniones(){
         Log.d(TAG, "buscarOpiniones: ");
 
